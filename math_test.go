17500d5870cdc0c00aa2a901b147c1646825c78e
Eduardo  Ximendes <ximendes.eduardo@gmail.com>
2022-07-26 21:58:13 -0300

    add function soma

---
+++ math_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+
+	total := Soma(15, 15)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: Resultado %d esperado: %d", total, 30)
+	}
+}
